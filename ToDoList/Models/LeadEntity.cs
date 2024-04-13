d074881d776ca4fd45f0fecf924daa77f5ee293d
project <world.wide.eunseo@gmail.com>
2024-04-13 18:27:43 +0900

    초기 커밋.

---
+++ ToDoList/Models/LeadEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Models
+{
+    public class LeadEntity
+    {
+        public int Id { get; set; }
+        public DateTime LeadDate { get; set; }
+        public required string LeadSource { get; set; }
+        public required string Name { get; set; }
+        public required string Mobile { get; set; }
+        public required string Email { get; set; }
+    }
+}
